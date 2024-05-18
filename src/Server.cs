0a47635650cf5957d00308ea9a5570166638512e
Rodolfo Penha <rodolfo.penha@gmail.com>
2024-05-18 17:22:11 -0300

    pass stage

--- src/Server.cs
+++ src/Server.cs
@@ -5,6 +5,8 @@ using System.Net.Sockets;
 Console.WriteLine("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
-TcpListener server = new TcpListener(IPAddress.Any, 6379);
+var server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
-server.AcceptSocket(); // wait for client
+var socket = server.AcceptSocket(); // wait for client
+
+await socket.SendAsync(@"+PONG\r\n"u8.ToArray(), SocketFlags.None);
