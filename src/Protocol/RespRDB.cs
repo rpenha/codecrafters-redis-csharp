0462db66b0189275c4e633116a37656086f9e78c
Rodolfo Penha <rodolfo.penha@gmail.com>
2024-05-23 22:40:58 -0300

    pass stage

--- src/Protocol/RespRDB.cs
+++ src/Protocol/RespRDB.cs
@@ -15,6 +15,6 @@ public sealed record RespRDB : RespValue
     {
         var value = new RespString($"FULLRESYNC {ServerInfo.GetReplId()} {ServerInfo.GetMasterReplOffset()}");
         var encoded = value.Encode();
-        return new ArraySegment<byte>([..encoded, .."$"u8, ..EmptyRDB, CR, LF]);
+        return new ArraySegment<byte>([..encoded, .."$88"u8, ..EmptyRDB, CR, LF]);
     }
 }
