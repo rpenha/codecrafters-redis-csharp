4882ef31a379d8184d4b7a901fc2e5f417bf567e
Rodolfo Penha <rodolfo.penha@gmail.com>
2024-05-23 22:18:35 -0300

    pass stage

--- src/Commands/Psync.cs
+++ src/Commands/Psync.cs
@@ -2,7 +2,6 @@ public sealed class Psync : Command
 {
     public override Task<RespValue> ExecuteAsync(CancellationToken cancellationToken)
     {
-        var value = $"FULLRESYNC {ServerInfo.GetReplId()} {ServerInfo.GetMasterReplOffset()}";
-        return Task.FromResult((RespValue)new RespString(value));
+        return Task.FromResult((RespValue)new RespRDB());
     }
 }
